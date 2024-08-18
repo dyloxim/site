0b7c7b44f51e81d4be77e4c63f244c183aba4f83
Joel Strouts <joelstrouts@gmail.com>
2024-08-18 21:26:06 +0100

    check for changed FS directly

--- src/IFS/UI/page/readOut/panel.tsx
+++ src/IFS/UI/page/readOut/panel.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { default as Function } from './function';
 import { Ctx } from '@IFS/UI/SharedUIState';
-import { I_transform } from "@IFS/types/mathematical";
+import { I_affine, I_transform } from "@IFS/types/mathematical";
 import { I_session } from "@IFS/types/state";
 
 
@@ -13,8 +13,19 @@ const Panel = ({session}: {session: I_session}) => {
     session.settings.FS.transforms
   );
 
+  const FStoString = (FS: I_transform[]) => {
+    let result = "";
+    FS.forEach(T => {
+      for(var propertyName in T) {
+        result += `${(T as I_affine)[(propertyName as "linear" | "translation")]}`;
+      }
+    })
+    console.log(result);
+    return result;
+  }
+
   useEffect(() => {
-    if(session.state.options.path == ctx.path) {
+    if(FStoString(transforms) != FStoString(session.settings.FS.transforms)) {
       setTransforms(session.settings.FS.transforms);
       if (session.state.inputSelected) {
         document.getElementById(`${session.state.inputSelected}`)!.focus()
