5ff050721290b904ae281ff251364ccf0687fbde
Joel Strouts <joelstrouts@gmail.com>
2024-08-25 14:41:20 +0100

    update version control to watch case differences

---
+++ app/ifs/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+import App from '@IFS/UI/page/app';
+import { defaultDisplay } from '@IFS/resources/presets/displayPresets';
+import { FunctionSystems } from '@IFS/resources/presets/FSPresets';
+import Link from 'next/link';
+import Script from 'next/script';
+
+export default function IFSPage() {
+
+  let preset = {
+    display: defaultDisplay,
+    FS: FunctionSystems.colorSpace
+  }
+
+  return (
+    <>
+      <App preset={preset} />
+      <hr style={{backgroundColor: "black", borderColor: "#333"}}/>
+      <div style={{padding: "1em"}}>
+        <Link href="IFS/about">🖇 About</Link>
+      </div>
+      <Script src="/webpack/IFS.bundle.js"/>
+    </>
+  )
+}
